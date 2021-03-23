c2debca757ef0edc576a142abacf67a68c980d56
Ecrane0107 <45076844+Ecrane0107@users.noreply.github.com>
2021-03-23 15:10:49 -0700

    Create linkedlisttesting.cpp

---
+++ linkedlisttesting.cpp
@@ -0,0 +1,37 @@
+// linkedlisttesting.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+class Node {
+public:
+    //value
+    int Value;
+    //pointer to next element/node
+    Node* Next;
+};
+
+void printList(Node*n) {
+    while(n != NULL) {
+        std::cout << n->Value << std::endl;
+        n = n->Next;
+    }
+}
+
+int main()
+{
+    Node* head = new Node();
+    Node* second = new Node();
+    Node* third = new Node();
+
+    head->Value = 1;
+    head->Next = second;
+    second->Value = 2;
+    second->Next = third;
+    third->Value = 3;
+    third->Next = NULL;
+
+    printList(head);
+
+    std::cout << "Test Passed" << std::endl;
+}
